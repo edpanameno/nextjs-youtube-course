4874c3cb5349fb18d07150419cee00d323d976e0
Ed Panameno <ed.panameno@gmail.com>
2025-05-25 07:47:46 -0700

    more updates as I continue to learn this new version of nextjs

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest) {
+export async function GET(request: NextRequest) {
     return NextResponse.json({ message: 'Hello, world!' });
 }
 
-export async function POST(req: NextRequest) {
-    const data = await req.json();
+export async function POST(request: NextRequest) {
+    const data = await request.json();
     const { name } = data;
     return NextResponse.json({ message: `Hello, ${name}! this was sent from the api route.`});
 }
